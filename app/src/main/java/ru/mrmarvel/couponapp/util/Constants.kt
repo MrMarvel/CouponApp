62aced509036eddb7ba95d6cd7835e5144bf97c4
MrMarvel <seregakkk999@yandex.ru>
2023-05-20 23:22:56 +0300

    Added NAV constants

--- app/src/main/java/ru/mrmarvel/couponapp/util/Constants.kt
+++ app/src/main/java/ru/mrmarvel/couponapp/util/Constants.kt
@@ -6,6 +6,8 @@ import ru.mrmarvel.couponapp.R
 public object Constants {
     const val NAV_SHOP_SCREEN = "shop"
     const val NAV_HOME_SCREEN = "home"
+    const val NAV_COUPON_CATEGORY_ARGUMENT = "id"
+    const val NAV_COUPON_CATEGORY_SCREEN = "coupon_category/{id}"
     val SHOP_TITLE_BUTTON = MainActivity.getAppResources()!!.getString(R.string.nav_button_shop_label)
     val HOME_TITLE_BUTTON = MainActivity.getAppResources()!!.getString(R.string.nav_button_home_label)
 }
