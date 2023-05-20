4088baf6025d4f24b3e3acac99d6599a6bd521a4
MrMarvel <seregakkk999@yandex.ru>
2023-05-20 14:30:21 +0300

    Added Room

--- app/src/main/java/ru/mrmarvel/couponapp/data/Coupon.kt
+++ app/src/main/java/ru/mrmarvel/couponapp/data/Coupon.kt
@@ -1,10 +1,14 @@
 package ru.mrmarvel.couponapp.data
 
+import androidx.room.Entity
 import java.util.Date
 
+@Entity
 data class Coupon (
     val id: ULong,
     var category: CouponCategory,
     var content: String,
-    var expiring_date: Date?
+    var expiring_date: Date?,
+    var added_by: User,
+var owned_by: User,
 )
