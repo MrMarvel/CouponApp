a0cde8d37e0494df37996b8b3a8807e7780a6c2e
MrMarvel <seregakkk999@yandex.ru>
2023-05-20 23:20:52 +0300

    Added Add button Setup Home list

--- app/src/main/java/ru/mrmarvel/couponapp/screens/CouponCategoryScreen.kt
+++ app/src/main/java/ru/mrmarvel/couponapp/screens/CouponCategoryScreen.kt
@@ -1,21 +1,17 @@
 package ru.mrmarvel.couponapp.screens
 
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.material3.FilledTonalButton
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import ru.mrmarvel.couponapp.data.CouponCategory
+import ru.mrmarvel.couponapp.data.models.CouponCategory
 import ru.mrmarvel.couponapp.data.DataProvider
 import ru.mrmarvel.couponapp.ui.HotCouponCategoryListItem
 import ru.mrmarvel.couponapp.viewmodels.SharedViewModel
--- app/src/main/java/ru/mrmarvel/couponapp/screens/ShopScreen.kt
+++ app/src/main/java/ru/mrmarvel/couponapp/screens/ShopScreen.kt
@@ -3,6 +3,13 @@ package ru.mrmarvel.couponapp.screens
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -12,6 +19,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import ru.mrmarvel.couponapp.ui.HotSection
 import ru.mrmarvel.couponapp.viewmodels.SharedViewModel
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ShopScreen(
     // shopScreenState:
@@ -21,8 +29,14 @@ fun ShopScreen(
         Modifier
             .fillMaxSize()
             .background(color = Color.Black)) {
-        Column {
-            HotSection()
+        Scaffold(floatingActionButton = {
+            FloatingActionButton(onClick = {  }) {
+                Icon(Icons.Filled.Add,"")
+            }
+        }) { padding ->
+            Column (Modifier.padding(padding)) {
+                HotSection()
+            }
         }
     }
 }
